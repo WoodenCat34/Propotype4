7cf2a6ccbf43cd07b02282735e91e73b20d7d3ce
WoodenCat34 <woodencat34@gmail.com>
2025-02-01 15:41:49 +0200

    Code: camera rotation

--- Assets/Course Library/Scripts/RotateCamera.cs
+++ Assets/Course Library/Scripts/RotateCamera.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    private float rotationSpeed = 100.0f;
+    public float horizontalInput; 
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, rotationSpeed * horizontalInput * Time.deltaTime);  
     }
 }
